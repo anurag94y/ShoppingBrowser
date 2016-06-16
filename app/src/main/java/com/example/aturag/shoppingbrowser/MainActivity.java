209dd5f4f14331bd493ff91bedf3304e835f5c7c
anurag94y <anurag94y@gmail.com>
2016-06-16 20:28:42 +0530

    First Commit

---
+++ app/src/main/java/com/example/aturag/shoppingbrowser/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.aturag.shoppingbrowser;
+
+import android.app.Activity;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.method.KeyListener;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private EditText mEdittext;
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEdittext = (EditText) findViewById(R.id.editText);
+
+        OpenUrl("https://www.facebook.com/");
+        mEdittext.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView tv, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE || actionId == KeyEvent.KEYCODE_ENTER) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(mEdittext.getWindowToken(), 0);
+                    OpenUrl(String.valueOf(mEdittext.getText()));
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void OpenUrl(String url) {
+        System.out.println(">>>>" + url);
+        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl(url);
+    }
+
+
+}
