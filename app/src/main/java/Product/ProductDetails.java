1e7f08b8ca9f75cf8ac81a295be09e954853fa09
paras885 <paras.meena885@gmail.com>
2016-06-24 02:19:10 +0530

    Added for mobile site

--- app/src/main/java/Product/ProductDetails.java
+++ app/src/main/java/Product/ProductDetails.java
@@ -30,34 +30,69 @@ public class ProductDetails {
     public ProductDetails(String Url_path, String Company_Name) throws IOException{
         switch (Company_Name) {
             case "flipkart":
-                Flipkart(Url_path);
+                Flipkart(Url_path, Company_Name);
                 break;
             case "amazon":
-                Amazon(Url_path);
+                Amazon(Url_path, Company_Name);
                 break;
             case "ebay":
-                Ebay(Url_path);
+                Ebay(Url_path, Company_Name);
                 break;
             case "snapdeal":
-                Snapdeal(Url_path);
+                Snapdeal(Url_path, Company_Name);
                 break;
         }
     }
 
-    public void Flipkart(String Url_path) throws IOException{
+    boolean IsDesktopSite(String Url_path, String Company_Name) {
+        int idx = -1;
+        // Is Desktop
+        switch (Company_Name) {
+            case "flipkart":
+                idx = Url_path.indexOf("www.flipkart");
+                break;
+            case "amazon":
+                idx = Url_path.indexOf("www.amazon");
+                break;
+            case "ebay":
+                idx = Url_path.indexOf("www.ebay");
+                break;
+            case "snapdeal":
+                idx = Url_path.indexOf("www.snapdeal");
+                break;
+        }
 
-        System.out.println("-------------------------Flipkart me aaya----------------------");
+        if(idx != -1) return true;
 
+        //Is Mobile site
+        switch (Company_Name) {
+            case "flipkart":
+                idx = Url_path.indexOf("m.flipkart");
+                break;
+            case "amazon":
+                idx = Url_path.indexOf("m.amazon");
+                break;
+            case "ebay":
+                idx = Url_path.indexOf("m.ebay");
+                break;
+            case "snapdeal":
+                idx = Url_path.indexOf("m.snapdeal");
+                break;
+        }
+
+        if(idx != -1) return false;
+        return true;
+    }
+
+    public void Flipkart(String Url_path, String Company_Name) throws IOException{
         doc = null;
         Paras_function(Url_path);
-        System.out.println("-------------------------lock variable true ho gya ---------------");
 
+        if(doc == null) return;
+
+        boolean is_desktop_site = IsDesktopSite(Url_path, Company_Name); // No need for flipkart
 
         Element product_deatils = doc.select("div.product-details").first();
-
-        //System.out.println("---------------------product_details----------------\n" + product_deatils);
-
-
 
         //Extract price div and then price of product
         Element price_div = product_deatils.select("div.prices").first();
@@ -69,46 +104,55 @@ public class ProductDetails {
 
     }
 
-    public void Amazon(String Url_path) throws IOException{
-
-        System.out.println("-------------------------Amazon me aaya----------------------");
+    public void Amazon(String Url_path, String Company_Name) throws IOException{
 
         doc = null;
         Paras_function(Url_path);
 
-        System.out.println("-------------------------lock variable true ho gya ---------------");
+        if(doc == null) return;
 
+        boolean is_desktop_site = IsDesktopSite(Url_path, Company_Name);  // Also dosen't matter :(
 
         product_name = doc.select("span#productTitle").first().text();
-        product_price = doc.select("div#price").first().select("span.a-color-price").first().text();;
+        product_price = doc.select("div#price").first().select("span.a-color-price").first().text();
     }
 
-    public void Ebay(String Url_path) throws IOException{
-
-        System.out.println("-------------------------Ebay me aaya----------------------");
+    public void Ebay(String Url_path, String Company_Name) throws IOException{
 
         doc = null;
         Paras_function(Url_path);
-        System.out.println("-------------------------lock variable true ho gya ---------------");
 
+        if(doc == null) return;
 
-        product_name = doc.select("h1#itemTitle").first().text();
-        product_price = doc.select("span#prcIsum").first().text();
+        boolean is_desktop_site = IsDesktopSite(Url_path, Company_Name);
+
+        if(is_desktop_site) {
+            product_name = doc.select("h1#itemTitle").first().text();
+            product_price = doc.select("span#prcIsum").first().text();
+        } else {
+            product_name = doc.select("span.vip-title").first().text();
+            product_price = doc.select("span.vip-price").first().text();
+        }
     }
 
-    public void Snapdeal(String Url_path) throws IOException {
-
-        System.out.println("-------------------------Snapdeal me aaya----------------------");
+    public void Snapdeal(String Url_path, String Company_Name) throws IOException {
 
         doc = null;
         Paras_function(Url_path);
-        System.out.println("-------------------------lock variable true ho gya ---------------");
-        Element product_details = doc.select("div.comp-product-description").first();
 
-        //System.out.println("---------------------product_details----------------\n"+ product_details);
+        if(doc == null) return;
 
-        product_name = product_details.select("h1").first().text();
-        product_price = product_details.select("span.payBlkBig").first().text();
+        boolean is_desktop_site = IsDesktopSite(Url_path, Company_Name);
+
+        if(is_desktop_site) {
+            Element product_details = doc.select("div.comp-product-description").first();
+            product_name = product_details.select("h1").first().text();
+            product_price = product_details.select("span.payBlkBig").first().text();
+        } else {
+            Element product_details = doc.select("div.product-details").first();
+            product_name = product_details.select("div.product-title").first().text();
+            product_price = product_details.select("span#sdPrice").first().text();
+        }
     }
 
     private void Paras_function(final String Url) {
@@ -117,7 +161,7 @@ public class ProductDetails {
             doc = Jsoup.connect(Url).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36").get();
            // System.out.println("doc doc " + Url + doc);
         } catch (Exception e) {
-            System.out.println("Error in Snapdeal Doc !!!!" +  e.getMessage());
+            System.out.println("Error in Doc !!!!" +  e.getMessage());
         }
     }
 
